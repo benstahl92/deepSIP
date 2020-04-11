b659818560cce47bc090d97988820690d7524864
Benjamin Stahl <benstahl92@gmail.com>
2020-04-10 23:55:19 -0700

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -43,6 +43,9 @@ templates_path = ['_templates']
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
+# to fix issue with RTD
+master_doc = 'index'
+
 
 # -- Options for HTML output -------------------------------------------------
 
